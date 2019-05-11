009570cc5c66cd1aeea8e58aea168b5762cb9aad
EyderACM <eyderacm@gmail.com>
2019-05-10 23:00:48 -0500

    add three functions

--- crudMatriz.c
+++ crudMatriz.c
@@ -4,28 +4,54 @@
 struct item{
     int ID;
     float price;
-    int cuantity;
+    int quantity;
 };
 
 typedef struct item Item;
 
-Item** inicialize(Item **array, int size);
+typedef struct {
+    Item **array;
+    int counter;
+    int maxSize;
+} ArrayList ;
+
+
+Item** inicialize(ArrayList content);
 Item* createItem();
-int addItem(Item** array, int counter, int maxSize);
-int searchID(Item** array, int maxSize);
-void update(Item** array, int maxSize);
-int erase(Item** array, int counter, int maxSize);
+int addItem(ArrayList content);
+int searchID(ArrayList content);
+void update(ArrayList content);
+int erase(ArrayList content);
+int compareTo(Item *firstItem, Item *secondItem);
+int swap(Item *itemOne, Item *itemTwo);
+
 int main(){
 
     int choice;
-
-    int counter = 0;
-    int maxSize = 0;
+    ArrayList content;
+    content.counter = 0;
+    content.maxSize = 0;
+    content.array = NULL;
     int flagSuccesAdd = 0;
-    Item** array = NULL;
     printf("Welcome, please type the size of the list:\n");
-    scanf("%d",&maxSize);
-    array = inicialize(array,maxSize);
+    scanf("%d",&content.maxSize);
+    content.array = inicialize(content);
+
+    Item *item1 = (Item*)malloc(sizeof(Item));
+    item1->ID = 1;
+    item1->price = 1;
+    item1->quantity = 1;
+    Item *item2 = (Item*)malloc(sizeof(Item));
+    item2->ID = 2;
+    item2->price = 2;
+    item2->quantity = 2;
+    printf("\nItem 1 \nID = %d\nPrice = %f\nQuantity = %d\n", item1->ID, item1->price, item1->quantity);
+    printf("\nItem 2 \nID = %d\nPrice = %f\nQuantity = %d\n", item2->ID, item2->price, item2->quantity);
+    swap(item1, item2);
+    printf("\n\nAfter swap\n");
+    printf("\nItem 1 \nID = %d\nPrice = %f\nQuantity = %d\n", item1->ID, item1->price, item1->quantity);
+    printf("\nItem 2 \nID = %d\nPrice = %f\nQuantity = %d\n", item2->ID, item2->price, item2->quantity);
+
     while (1){
         printf("Welcome to the product manager\n");
         printf("1. Create an element \n");
@@ -40,27 +66,27 @@ int main(){
         switch (choice)
         {
             case 1:
-            flagSuccesAdd = addItem(array,counter,maxSize);
+            flagSuccesAdd = addItem(content);
             if(flagSuccesAdd == 1){
                 printf("\nSuccess!\n");
-                printf("ID: %d\n", array[counter]->ID);
-                printf("Cuantity: %d\n", array[counter]->cuantity);
-                printf("Price: %f\n\n", array[counter]->price);
-                counter = counter + 1;
+                printf("ID: %d\n", content.array[content.counter]->ID);
+                printf("Quantity: %d\n", content.array[content.counter]->quantity);
+                printf("Price: %f\n\n", content.array[content.counter]->price);
+                content.counter = content.counter + 1;
             } else {
                 printf("Hmm, it seems that there's no more room in the list.\n");
             }
                 break;
             case 2:
-            update(array,maxSize);
+            update(content);
                 break;
             case 3:
-            searchID(array,maxSize);
+            searchID(content);
                 break;
             case 4:
-            flagSuccesAdd = erase(array,counter,maxSize);
+            flagSuccesAdd = erase(content);
             if(flagSuccesAdd == 1){
-                counter = counter - 1;
+                content.counter = content.counter - 1;
             }
                 break;
             case 5:
@@ -75,9 +101,9 @@ int main(){
     }
 }
 
-Item** inicialize(Item **array, int size){
-	array = (Item**)malloc(sizeof(Item**)*size);
-	return array;
+Item** inicialize(ArrayList content){
+	content.array = (Item**)malloc(sizeof(Item**)*content.maxSize);
+	return content.array;
 }
 
 Item* createItem(){
@@ -94,47 +120,47 @@ Item* createItem(){
     Item* element = (Item*)malloc(sizeof(Item));
     element->ID = id;
     element->price = cost;
-    element->cuantity = nProducts;
+    element->quantity = nProducts;
     return element;
 }
 
-int addItem(Item** array, int counter, int maxSize){
+int addItem(ArrayList content){
     int flag = 0;
-    if(counter < maxSize){
+    if(content.counter < content.maxSize){
         Item* element = createItem();
-        *(array+counter) = element;
+        *(content.array+content.counter) = element;
         flag = 1;
     }
     return flag;
 }
 
-int searchID(Item** array, int maxSize){
+int searchID(ArrayList content){
     int counter = 0;
     int target = 0;
     printf("Enter the ID of the target product:\n");
     scanf("%d",&target);
-    while(counter!=maxSize){
-        if((*(array+counter))->ID == target){
+    while(content.counter < content.maxSize){
+        if(content.array[counter]->ID == target){
             printf("\nFound:\n");
-            printf("ID: %d\n", array[counter]->ID);
-            printf("Cuantity: %d\n", array[counter]->cuantity);
-            printf("Price: %f\n\n", array[counter]->price);
+            printf("ID: %d\n", content.array[counter]->ID);
+            printf("Quantity: %d\n", content.array[counter]->quantity);
+            printf("Price: %f\n\n", content.array[counter]->price);
             break;
         }
     counter = counter + 1;
     }
-    if(counter == maxSize){
+    if(content.counter == content.maxSize){
         printf("\nSorry, that ID is not correct.\n");
         counter = -1;
     }
     return counter;
 }
 
-void update(Item** array, int maxSize){
+void update(ArrayList content){
     int id;
     int nProducts;
     float cost;
-    int target = searchID(array,maxSize);
+    int target = searchID(content);
     if(target!=-1){
         printf("Enter the new ID of the product:\n");
         scanf("%d",&id);
@@ -142,30 +168,51 @@ void update(Item** array, int maxSize){
         scanf("%d",&nProducts);
         printf("Enter the new cost of the product:\n");
         scanf("%f",&cost);
-        (*(array+target))->ID = id;
-        (*(array+target))->price = cost;
-        (*(array+target))->cuantity = nProducts;
+        (*(content.array+target))->ID = id;
+        (*(content.array+target))->price = cost;
+        (*(content.array+target))->quantity = nProducts;
         printf("\nNew Information:\n");
-        printf("ID: %d\n", array[target]->ID);
-        printf("Cuantity: %d\n", array[target]->cuantity);
-        printf("Price: %f\n\n", array[target]->price);
+        printf("ID: %d\n", content.array[target]->ID);
+        printf("Quantity: %d\n", content.array[target]->quantity);
+        printf("Price: %f\n\n", content.array[target]->price);
     }
 }
-int erase(Item** array, int counter, int maxSize){
+int erase(ArrayList content){
     int flag = 0;
-    int target = searchID(array,maxSize);
+    int target = searchID(content);
     if(target!=-1){
-        free(*(array+target));
+        free(*(content.array+target));
         printf("The item has been erased.\n");
-        if(target!=maxSize-1){
-            while(target!=counter){
-                (*(array+target))->ID = (*(array+target+1))->ID;
-                (*(array+target))->cuantity = (*(array+target+1))->cuantity;
-                (*(array+target))->price = (*(array+target+1))->price;
+        if(target!=content.maxSize-1){
+            while(target!=content.counter){
+                (*(content.array+target))->ID = (*(content.array+target+1))->ID;
+                (*(content.array+target))->quantity = (*(content.array+target+1))->quantity;
+                (*(content.array+target))->price = (*(content.array+target+1))->price;
                 target = target + 1;
             }
-            free(*(array+target));
+            free(*(content.array+target));
         }
+        flag = 1;
+    }
+    return flag;
+}
+
+int compareTo(Item *firstItem, Item *secondItem){
+    int comparasion = 0;
+    if(firstItem->price > secondItem->price){
+        comparasion = 1;
+    }else if(firstItem->price < secondItem->price) {
+        comparasion = -1;
+    }
+    return comparasion;
+}
+
+int swap(Item *itemOne, Item *itemTwo){
+    int flag = 0;
+    if(itemOne != NULL && itemTwo != NULL){
+        Item temp = *itemOne;
+        *itemOne = *itemTwo;
+        *itemTwo = temp;
         flag = 1;
     }
     return flag;
